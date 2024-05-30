493fa1a477a5abba81679b7ee11d35479f4eb063
Chris Nelson <superchrisnelson@gmail.com>
2024-05-30 15:46:01 -0400

    fix swagger_ui

--- lib/phoenix_swagger/plug/swaggerui.ex
+++ lib/phoenix_swagger/plug/swaggerui.ex
@@ -44,7 +44,29 @@ defmodule PhoenixSwagger.Plug.SwaggerUI do
       <div id="swagger-ui"></div>
       <script src="./swagger-ui-bundle.js" charset="UTF-8"> </script>
       <script src="./swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
-      <script src="./swagger-initializer.js" charset="UTF-8"> </script>
+      <script>
+      window.onload = function() {
+        //<editor-fold desc="Changeable Configuration Block">
+
+        // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+        window.ui = SwaggerUIBundle({
+          url: '<%= spec_url %>',
+          dom_id: '#swagger-ui',
+          deepLinking: true,
+          presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+          ],
+          plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+          ],
+          layout: "StandaloneLayout"
+        });
+
+        //</editor-fold>
+      };
+
+      </script>
     </body>
   </html>
   """
